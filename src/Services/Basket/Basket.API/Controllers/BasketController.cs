0ebd2400b0c516ec63bf21f970deda0100053b3d
vamsi tokala <vamsitokala@live.com>
2022-10-20 16:32:44 +0000

    basked api -implimented  ibaset repository

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    /*v1/[controller] havingcontroller in [], it is going to get the catalog controller and remove controller and give only catalog
+*output is "api/v1/Catalog
+ */
+    [ApiController]
+    [Route("api/v1/[controller]")]
+
+
+
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+
+        }
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);//fatching basekt info from shoppingcart object
+        Task DeleteBasket(string userName); //user name will be key of our key value structure in dictionary database
+    }
+}
